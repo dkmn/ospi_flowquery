4a1cc805b3caa7124fc13c0ed516f572b2bdd066
dkmn <david@radardoc.net>
2019-08-12 10:15:33 -0700

    Adding formatting to numeric strings.

--- read_flow_pin.py
+++ read_flow_pin.py
@@ -24,7 +24,7 @@ last_value = GPIO.input(GPIO_PIN_NUMBER)
 time_window_begin = time_window_short_begin = time_window_end = overall_start_time = time.time()	# initialize to zero-width window, current time
 volume_multiplier = 1		# gal/cycle (cycle = 2 transitions)
 
-print ("Overall start time: ", overall_start_time)
+print ("Overall start time: %.2f sec" % overall_start_time)
 
 for loop_counter in range(0, 50000000):
     current_value = GPIO.input(GPIO_PIN_NUMBER)
@@ -33,19 +33,19 @@ for loop_counter in range(0, 50000000):
         transition_count += 1
         if transition_count % 2 == 0:
             nowish = time.time()
-            print ("Loop counter: ", loop_counter)
+            print ("Loop counter: %d" %  loop_counter)
             time_window_end = nowish
-            print ("Now: ", nowish, " Window start: ", time_window_begin)
-            print ("Now: ", nowish, " Short window start: ", time_window_short_begin)
-            est_flow_rate_window_recent = (60.0/(nowish - time_window_short_begin)) * (volume_multiplier / 2.0)
+            print ("Now: %.2f Window start: %.2f" % (nowish, time_window_begin))
+            print ("Now: %.2f Short window start: %.2f" %  (nowish,time_window_short_begin))
+            est_flow_rate_window_recent = (60.0/(nowish - time_window_short_begin)) * (volume_multiplier)
             est_flow_rate_window_total = (60.0/(nowish - time_window_begin)) * transition_count * (volume_multiplier / 2.0)
             total_flow_to_date = transition_count * volume_multiplier / 2.0
-            print ("Estimated flow rate recent (gal/min): ", est_flow_rate_window_recent)
-            print ("Estimated flow rate total (gal/min): ", est_flow_rate_window_total)
-            print ("Total volume to date: ", total_flow_to_date)
+            print ("Estimated flow rate recent (gal/min): %.2f" % est_flow_rate_window_recent)
+            print ("Estimated flow rate total (gal/min): %.2f" % est_flow_rate_window_total)
+            print ("Total volume to date: %.2f" % total_flow_to_date)
             avg_flow_rate_to_date = (60.0 * total_flow_to_date) / (nowish - overall_start_time)
-            print ("Average flow rate since script start: ", avg_flow_rate_to_date)
-            print ("Current transition count: ", transition_count)
-            print ("Current value: ", current_value)
+            print ("Average flow rate since script start: %.2f" %  avg_flow_rate_to_date)
+            print ("Current transition count: %d" % transition_count)
+            print ("Current value: %d" % current_value)
             time_window_short_begin = nowish
     time.sleep(0.25) 
